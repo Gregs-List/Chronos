4b720cad0fedbe3c93ca71ba0f3b64017613404b
GregWWalters <GregWWalters@gmail.com>
2013-10-13 02:22:16 -0500

    began script for populating type lookup tables

---
+++ database/populateTypes.sql
@@ -0,0 +1,31 @@
+# mysql -u root -p --local-infile GregsList
+use GregsList;
+
+# populate the ConditionLookup table
+INSERT INTO ConditionLookup(0, "other");
+INSERT INTO ConditionLookup(1, "new");
+INSERT INTO ConditionLookup(2, "excellent");
+INSERT INTO ConditionLookup(3, "light wear");
+INSERT INTO ConditionLookup(4, "normal wear");
+INSERT INTO ConditionLookup(5, "damaged");
+
+# populate the BikeType table
+INSERT INTO BikeType(0, "other");
+INSERT INTO BikeType(1, "road");
+INSERT INTO BikeType(2, "city");
+INSERT INTO BikeType(3, "mountain");
+INSERT INTO BikeType(4, "cruiser");
+INSERT INTO BikeType(5, "hybrid");
+INSERT INTO BikeType(6, "unicycle");
+INSERT INTO BikeType(7, "electric");
+
+# populate the BookType table
+INSERT INTO BookType(0, "other");
+INSERT INTO BookType(1, "textbook");
+INSERT INTO BookType(2, "literature");
+INSERT INTO BookType(3, "poetry");
+INSERT INTO BookType(4, "reference");
+INSERT INTO BookType(5, "nonfiction");
+INSERT INTO BookType(6, "graphic novel");
+
+
