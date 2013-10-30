340d5a3321714e1ed7627d4b184d54369e168b82
Amanda Doyle <adoyle@smu.edu>
2013-10-30 17:27:36 -0500

    Created framework for HTTP request within an asynchronous shell and tested with a dummy PHP file - connection successful

--- gregsList/src/com/example/gregslist/LoginActivity.java
+++ gregsList/src/com/example/gregslist/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.gregslist;
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -14,9 +15,12 @@ import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
+import android.R.string;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
@@ -43,35 +47,42 @@ public class LoginActivity extends Activity {
     }
     
     public void sendMessage(View view) {
-    	Context context = getApplicationContext();
+    	/*
     	EditText email = (EditText)findViewById(R.id.email);
     	EditText password = (EditText)findViewById(R.id.password);
     	String text = new StringBuilder().append(email.getText()).append("\n").append(password.getText()).toString();
-    	int duration = Toast.LENGTH_SHORT;
-    	Toast toast = Toast.makeText(context, text, duration);
-    	toast.show();
-    	/*
-    	HttpClient httpClient = new DefaultHttpClient();  
-    	String url = "http://lyle.smu.edu/~adoyle/3345/a7/images.json";
-    	HttpGet httpGet = new HttpGet(url);
-    	try {
-    	    HttpResponse response = httpClient.execute(httpGet);
-    	    StatusLine statusLine = response.getStatusLine();
-    	    if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
-    	        HttpEntity entity = response.getEntity();
-    	        ByteArrayOutputStream out = new ByteArrayOutputStream();
-    	        entity.writeTo(out);
-    	        out.close();
-    	        String responseStr = out.toString();
-    	        Log.d("response",responseStr);
-    	        // do something with response 
-    	    } else {
-    	        // handle bad response
-    	    }
-    	} catch (ClientProtocolException e) {
-    	    // handle exception
-    	} catch (IOException e) {
-    	    // handle exception
-    	}*/
+    	*/
+        new DownloadFilesTask().execute("http://ec2-54-242-12-103.compute-1.amazonaws.com/test.php");
 }
+    
+    private class DownloadFilesTask extends AsyncTask<String, Void, String> {
+        
+    	protected String doInBackground(String... urls) {
+    		String url = urls[0];
+    		String result = " ";
+        Log.d("check","doInBackground has been called");
+        	HttpClient client = new DefaultHttpClient();
+        	HttpGet get = new HttpGet(url);
+        	try {
+        		HttpResponse response = client.execute(get);
+        		HttpEntity entity = response.getEntity();
+        			if (null != entity) {
+        				result = EntityUtils.toString(entity);
+        			}
+        	} catch (ClientProtocolException e) {
+        		e.printStackTrace();
+        	} catch (IOException e) {
+        		e.printStackTrace();
+        	}
+           return result;
+        }
+
+        protected void onPostExecute(String result) {
+        	Context context = getApplicationContext();
+        	int duration = Toast.LENGTH_SHORT;
+        	Toast toast = Toast.makeText(context, result, duration);
+        	toast.show();
+        }
+    }
+ 
 }
