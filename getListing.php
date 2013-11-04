5bb10f3eccef56a99be552313e73cc64d055f9a7
rsantoski <rsantoski@smu.edu>
2013-11-03 23:11:16 -0600

    added get listing php

---
+++ getListing.php
@@ -0,0 +1,24 @@
+<?php
+	session_start();
+	$con = mysql_connect("localhost", "listAdmin", "hermes");
+	if(!$con)
+	{
+		die('Could not connect: ' . mysql_error());
+	}
+	
+	mysql_select_db("GregsList", $con)
+		or die("Unable to select database:" . mysql_error());
+
+	$result = mysql_query("SELECT * FROM Listings WHERE userID = '$_SESSION[userID]' ORDER BY dateListed DESC");
+
+	#converts to json
+	$rows = array();
+	while($r = mysql_fetch_assoc($result)) 
+	{
+    		$rows[] = $r;
+	}
+
+	#If you want to see if correct json is printing use ---> print json_encode($rows);
+	
+	return json_encode($rows);
+?>
