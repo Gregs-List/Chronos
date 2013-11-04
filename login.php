01f629c7e9e8c05f4aa97e0e157a2c9d1e1c72fb
Amanda Doyle <adoyle@smu.edu>
2013-11-03 18:45:49 -0600

    fixed Login functionality

--- login.php
+++ login.php
@@ -1,30 +1,34 @@
 <?php
-	$con = mysql_connect("localhost", "gregslist", "greg");
-	if (!$con)
-	{
-		die('Could not connect: ' . mysql_error());
-	}
-	//Function to check the log in of a user
-	function login(){
-    echo 'meow';
-	$query = "select userID from Users where email ='$_GET[email]'";
-	$result = mysql($query);
-	while ($row = mysql_fetch_assoc($id_query_result)){
-        $id = $row ['userID'];
+
+    $con = mysql_connect("localhost", "listAdmin", "hermes");
+    if (!$con)
+    {
+        die('Could not connect: ' . mysql_error());
+    }
+
+    mysql_select_db("GregsList", $con) 
+        or die("Unable to select database:" . mysql_error());
+
+        error_reporting(E_ALL);
+        ini_set('display_errors', '1');
+    //Function to check the log in of a user
+    $query = "select userID from Users where email ='$_GET[email]'";
+    $result = mysql_query($query);
+    while ($row = mysql_fetch_assoc($result)){
+        $id = $row['userID'];
     }
     session_start();
     $_SESSION['userID']=$id;
-	$query = "select pword from Users where userID ='$id'";
+    $query = "select pword from Users where userID =$id";
     $result = mysql_query($query);
     while ($row = mysql_fetch_assoc($result)){
-    	$database_password = $row ['pword'];
+        $database_password = $row ['pword'];
     }
     if($_GET['password'] != $database_password){
-   		header()//Enter webpage to show if the login credentials are invalid
+        header('Location: index.html');//Enter webpage to show if the login credentials are invalid
     }
     else{
-   		header()//Enter webpage to show if login credentials are valid
+        header('Location: home.html');//Enter webpage to show if login credentials are valid
         }
     mysql_close($con);
-	}
 ?>
