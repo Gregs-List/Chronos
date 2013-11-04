b81c4dcda2b8ae4798b350fdefe97e67161aaca5
rsantoski <rsantoski@smu.edu>
2013-11-03 23:25:08 -0600

    Fixed add listing php

--- addListing.php
+++ addListing.php
@@ -1,7 +1,7 @@
 <?php
 
 	session_start();
-	$con = mysql_connect("localhost", "listAdmin", "hermes");
+	$con = mysql_connect("localhost", "gregslist", "greg");
 	if(!$con)
 	{
 		die('Could not connect: ' . mysql_error());
@@ -13,10 +13,9 @@
 	$category = $_POST['category'];
 	$description = $_POST['description'];
 	$itemName = $_POST['itemName'];
-	echo $_SESSION['userID'];
 	$query = "INSERT INTO Listings VALUES(NULL, '$_SESSION[userID]', 
 		'$_POST[title]', NULL, '$_POST[category]', '$_POST[description]')";
 	mysql_query($query);
-	$query = "INSERT INTO Miscellaneous VALUES(mysql_insert_id(), '$_POST[itemName]')";
+	$query = "INSERT INTO Miscellaneous VALUES(last_insert_id(), '$_POST[itemName]')";
 	mysql_query($query);
 ?>
