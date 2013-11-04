5e543826b34075cfcb986dab6ccf6484be70ccf0
rsantoski <rsantoski@smu.edu>
2013-11-03 20:46:38 -0600

    Added css for account

--- createListing.js
+++ createListing.js
@@ -1,9 +1,8 @@
-$(document).click(function(){
+$(document).click(
 	$('#newListing').click(function(e){
 		console.log('test');
 		var xmlHttp = getXMLHttp();
 		xmlHttp.open("GET", "phpAPI.php", true);
 		xmlHttp.send('$_GET[userID]', '$_GET[title]', '$_GET[category]',
 			'$_GET[description]', '$_GET[itemName]')
-	});
 });
