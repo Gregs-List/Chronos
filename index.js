dfe0aef420d9b93b9b8c2923afbefd91423ab98e
Carter <csdewey2@gmail.com>
2013-11-08 13:18:09 -0600

    fixed smu emai sign up

--- index.js
+++ index.js
@@ -7,18 +7,11 @@ $(document).click(function(){
 			alert("Your passwords don't match");
 		}
 		var email = $('#newEmail').val();
-		var len = email.length;
-<<<<<<< HEAD
+
 		var smu = email.substring(len-7,len);
 		if(smu != 'smu.edu'){
 			e.preventDefault();
 			alert("You must use a valid SMU email address.");
-=======
-		var smu = email.substring(len-8,len);
-//		if(smu != 'smu.edu'){
-//			e.preventDefault();
-//			alert("You must use a valid SMU email address.");
->>>>>>> e317cdad50c5a6422b1f075be2e6f0211662c1e2
 		}
 	});
 });
