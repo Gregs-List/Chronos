7d8411a7f78e5c0233aa072f4d686ec39ce6d76a
GregWWalters <GregWWalters@gmail.com>
2013-11-08 19:29:51 -0600

    Wrapped inserts into Listing and Miscellaneous in a transaction

--- addListing.php
+++ addListing.php
@@ -13,22 +13,19 @@
 	$category = $_GET['category'];
 	$description = $_GET['description'];
 	$itemName = $_GET['itemName'];
-	$query = "INSERT INTO Listings VALUES(NULL, '$_SESSION[userID]', 
-		'$_GET[title]', NULL, '$_GET[category]', '$_GET[description]')";
-	mysql_query($query);
-	if(!query)
+	
+	$query = "START TRANSACTION; \n";
+	$query .= "INSERT INTO Listings VALUES(NULL, '$_SESSION[userID]', 
+		'$_GET[title]', NULL, '$_GET[category]', '$_GET[description]'); \n";
+	$query .= "INSERT INTO Miscellaneous VALUES(last_insert_id(), '$_GET[itemName]'); \n";
+	$query .= "COMMIT;";
+	$ins=mysql_query($query);
+	if(!$ins)
 	{
 		$message = 'Database insert failed: ' . mysql_error() . "\n";
     $message .= 'Whole query: ' . $query;
     die($message);
 	}
-	$query = "INSERT INTO Miscellaneous VALUES(last_insert_id(), '$_GET[itemName]')";
-	mysql_query($query);
-	if(!query)
-	{
-		$message = 'Database insert failed: ' . mysql_error() . "\n";
-    $message .= 'Whole query: ' . $query;
-    die($message);
-	}
+
 	Header('Location: home.html');
 ?>
