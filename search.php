b2b24db4743013063e2196c49da94220a2bf104e
mwinslow@smu.edu <mwinslow@smu.edu>
2013-11-15 00:23:33 -0600

    Added new functions for sort by category and price

--- search.php
+++ search.php
@@ -25,10 +25,30 @@
 	print json_encode($rows);
 	#return json_encode($rows);
 
-	function sortByCategory()
+	function sortByCategoryAndPrice()
 	{
-		$category = $_POST['category']; 
-		$query = "SELECT * FROM Listings WHERE category='$category' and title or description LIKE '%iPhone 4%'";
+		if(isset ($_GET['category'] && $_GET['price'])
+		{
+			# $category = $_POST['category']; 
+			# $price = $_POST['price'];
+
+			$query = "SELECT * FROM Listings WHERE category='$category' and title or description LIKE '%iPhone 4%' HAVING price <= '$price'";
+		}
+		elseif($_GET['category'])
+		{
+			# $category = $_POST['category'];
+
+			$query = "SELECT * FROM Listings WHERE category='$category' and title or description LIKE '%iPhone 4%'";
+		}
+		elseif($_GET['price'])
+		{
+			# $price = $_POST['price'];
+			
+			$query = "SELECT * FROM Listings WHERE price <= '$price' and title or description LIKE '%iPhone 4%'";
+			
+		}
+		
+		
 		$result = mysql_query($query);
 		
 		#converts to json
@@ -37,6 +57,14 @@
 		{
     			$rows[] = $r;
 		}
+
+		print json_encode($rows);
+		#return json_encode($rows);
+	}
+
+	function sortByCategory()
+	{
+		$price = $
 	}
 
 ?>
