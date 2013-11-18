2b43f9a46e25098cd8397f3708f8a2c28a83cd09
Carter <csdewey2@gmail.com>
2013-11-17 23:14:09 -0600

    Added fields for each category to createListing

--- createListing.js
+++ createListing.js
@@ -1,8 +1,41 @@
-$(document).click(
-	$('#newListing').click(function(e){
-		console.log('test');
+$(document).ready(function(){
+	/*$('#newListing').click(function(e){
 		var xmlHttp = getXMLHttp();
 		xmlHttp.open("GET", "phpAPI.php", true);
 		xmlHttp.send('$_GET[userID]', '$_GET[title]', '$_GET[category]',
 			'$_GET[description]', '$_GET[itemName]')
+	});*/
+
+	$('#category').change(function(e){
+		var text = $('#category :selected').text();
+		if(text == 'Books'){
+			hideFields();
+			$('#bookFields').css('display','Block');
+		}
+		else if (text == 'Furniture'){
+			hideFields();
+			$('#furnitureFields').css('display','Block');
+		}
+		else if (text == 'Electronics'){
+			hideFields();
+			$('#electronicFields').css('display','Block');
+		}
+		else if (text == 'Bikes'){
+			hideFields();
+			$('#bikeFields').css('display','Block');
+		}
+		else if (text == 'Misellanious'){
+			hideFields();
+		}
+	});
+
+
 });
+
+
+function hideFields(){
+	$('#bookFields').css('display','none');
+	$('#furnitureFields').css('display','none');
+	$('#electronicFields').css('display','none');
+	$('#bikeFields').css('display','none');
+}
--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-$(document).click(function(){
+$(document).ready(function(){
 	$('#signUp').click(function(e){
 
 		var email = $('#newEmail').val();
-		var smu = email.substring(len-7,len);
+		var len = email.length;
+		var smu = "tcu.edu" //just a holder string the in inheiently wrong
+		if(len > 6)
+			smu = email.substring(len-7,len);
+
+		var p1 = $('#newPassword').val();
+		var p2 = $('#reEnterPassword').val();
+
+
 		if(smu != 'smu.edu'){
 			e.preventDefault();
 			alert("You must use a valid SMU email address.");
 		}
 
-		var p1 = $('#newPassword').val();
-		var p2 = $('#reEnterPassword').val();
+		
 		else if (p1 != p2){
 			e.preventDefault();
 			alert("Your passwords don't match");
 		}
 
 		else{
-			alert("Sign Up Complete. You may now login.")
+			alert("Sign Up Complete. You may now login.");
 		}
 
 	});
