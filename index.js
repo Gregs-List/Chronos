5d258ab96ddf1b7b3847338ee8c80e2dcf5c6d1c
Carter Dewey <cdewey@smu.edu>
2013-11-17 19:08:28 -0600

    Create two new HTML files and added a few updates

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 $(document).click(function(){
 	$('#signUp').click(function(e){
-		var p1 = $('#newPassword').val();
-		var p2 = $('#reEnterPassword').val();
-		if (p1 != p2){
-			e.preventDefault();
-			alert("Your passwords don't match");
-		}
-		var email = $('#newEmail').val();
 
+		var email = $('#newEmail').val();
 		var smu = email.substring(len-7,len);
 		if(smu != 'smu.edu'){
 			e.preventDefault();
 			alert("You must use a valid SMU email address.");
 		}
+
+		var p1 = $('#newPassword').val();
+		var p2 = $('#reEnterPassword').val();
+		else if (p1 != p2){
+			e.preventDefault();
+			alert("Your passwords don't match");
+		}
+
+		else{
+			alert("Sign Up Complete. You may now login.")
+		}
+
 	});
 });
