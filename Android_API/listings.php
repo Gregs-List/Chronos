ad9e8b521baa84378adc876078cb8d1f85e3629e
Amanda <adoyle@smu.edu>
2013-11-19 12:12:47 -0600

    php functionality changed to output the most recent ten listings

--- Android_API/listings.php
+++ Android_API/listings.php
@@ -12,22 +12,26 @@
 
     $query = "select * from Listings order by listingID DESC";
     $result = mysql_query($query);
-
     $info = array();
     $counter = 0;
     while ($row = mysql_fetch_assoc($result)){
         if ($counter < 10) {
+        $listingID = $row['listingID'];
+        $userListingID = $row['userID'];
         $title = $row['title'];
-        array_push($info,$title);
+        $dateListed = $row['dateListed'];
         $category = $row['category'];
-        array_push($info,$category);}
+        $price = $row['price'];
+        $description = $row['description'];
+        $listing = array("listingID" => $listingID, "userListingID" =>$userListingID, "title" => $title, "dateListed" => $dateListed, "category" => $category, "price" => $price, "description" => $description);
+        array_push($info, $listing);
         $counter++;
     }
+}
+    $full_array= array("listings"=>$info);
 
-    foreach ($info as $value) {
-        echo $value;
-        echo "#";
-    }
+    $json = json_encode($full_array);
+    echo $json;
     
     mysql_close($con);
     ?>
