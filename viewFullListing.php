03d6c4a4cce4c9a1fc3283a3061ceef034c0fbdc
GregWWalters <GregWWalters@gmail.com>
2013-11-18 21:17:38 -0600

    saving progress on first category print statements

--- viewFullListing.php
+++ viewFullListing.php
@@ -1,6 +1,8 @@
 <?PHP
 
-#retreive information from Listings and <Category> tables. 
+# retreive information from Listings and <Category> tables.
+# future work:
+# if statements to prevent subtitles for null fields
 
 function retrieveFromAll() 
 {
@@ -16,24 +18,93 @@ function retrieveFromAll()
 	$listingID = $_GET['listingID'];
 	$category = $_GET['category'];
 
+// Query database for Listing information
 	$listQuery = "SELECT title, dateListed, price, fullName, description FROM Listings";
+	$result1 = mysql_query($listQuery);
+	if(!$result1)
+	{
+		$message = 'Database query failed: ' . mysql_error() . "\n";
+    $message .= 'Query: ' . $listQuery;
+    die($message);
+	}
+// store results in an array
+	$listing = mysql_fetch_array($result1, MYSQL_ASSOC);
+// print results to screen
+	echo "Title: {$listing[title]}<br>
+Date listed: {$listing[dateListed]}<br>
+Price: {$listing[price]}<br>
+Contact: {$listing[fullName]}<br>";
 
 
+// Print Bike information, if Bike
 	if($category=="Bikes")
 	{
-		$catQuery .= "SELECT bikeTypeID, make, model FROM Bikes WHERE listingID = '$listingID'";
-	}
-	
-	if($category=="Books")
+	$catQuery .= "SELECT bikeTypeID, make, model FROM Bikes WHERE listingID = '$listingID'";
+	$result2 = mysql_query($catQuery);
+//  error if query fails
+	if(!$result2)
 	{
-		$catQuery .= "SELECT bookTypeID, title, author, isbn, assignedCourse, conditionID from Books WHERE listingID = '$listingID'";
+		$message = 'Database query failed: ' . mysql_error() . "\n";
+    $message .= 'Query: ' . $catQuery;
+    die($message);
+	}
+// Convert to array and print
+	$cat = mysql_fetch_array($result2, MYSQL_ASSOC);
+	$bikeType = mysql_query("SELECT bikeType from BikeType where bikeTypeID = {$cat[bikeTypeID]};");
+	echo "Type: $bikeType<br>
+Make: {$cat[make]}<br>
+Model: {$cat[model]}<br>";
 	}
 
+
+// Print Book information, if Book	
+	if($category=="Books")
+	{
+	$catQuery .= "SELECT bookTypeID, title, author, isbn, assignedCourse, conditionID from Books WHERE listingID = $listingID;";
+	$result2 = mysql_query($catQuery);
+//  error if query fails
+	if(!$result2)
+	{
+		$message = 'Database query failed: ' . mysql_error() . "\n";
+    $message .= 'Query: ' . $catQuery;
+    die($message);
+	}
+// convert to array and print
+	$cat = mysql_fetch_array($result2, MYSQL_ASSOC);
+	$bookType = mysql_query("SELECT bookType from BookType where bookTypeID = {$cat[bookTypeID]};");
+	$condition = mysql_query("SELECT itemCondition FROM ConditionLookup where conditionID = {$cat[conditionID]};");
+	echo "Type: $bookType<br>
+Book title: {$cat[title]}<br>
+Author: {$cat[author]}<br>
+ISBN: {$cat[isbn]}<br>
+Assigned Course: {$cat[assignedCourse]}<br>
+Condition: $condition<br>";
+	}
+
+
+// Print Electronics information, if Electronics
 	if($category=="Electronics")
 	{
 		$catQuery .= "SELECT electronicsTypeID, make, model, size FROM Electronics WHERE listingID = '$listingID'";
+	$result2 = mysql_query($catQuery);
+//  error if query fails
+	if(!$result2)
+	{
+		$message = 'Database query failed: ' . mysql_error() . "\n";
+    $message .= 'Query: ' . $catQuery;
+    die($message);
+	}
+// convert to array and print
+	$cat = mysql_fetch_array($result2, MYSQL_ASSOC);
+	$electronicsType = mysql_query("SELECT electronicsType FROM ElectronicsType WHERE electronicsTypeID = {$cat[electronicsType]};");
+	echo "Type: $electronicsType<br>
+Make: {$cat[make]}<br>
+Model: {$cat[model]}<br>
+Size: {$cat[size]}<br>";
 	}
 
+
+// Print Furniture, if furniture
 	if($category=="Furniture")
 	{
 		$catQuery .= "SELECT furnitureTypeID, conditionID FROM Furniture WHERE listingID = '$listingID'";
@@ -54,18 +125,9 @@ function retrieveFromAll()
 		$catQuery .= "SELECT leavingFrom, goingTo, departureDate, departureTime, returnDate, returnTime FROM Rides WHERE listingID = '$listingID'";
 	}
 
-/*	$query = "START TRANSACTION;\n";
-	$query.=$listQuery . '\n';
-	$query.=$catQuery . '\n';
-	$query.="\nCOMMIT;"
-	$ins=mysql_query($query);
-	if(!$ins)
-	{
-		$message = 'Database insert failed: ' . mysql_error() . "\n";
-    $message .= 'Whole statement: ' . $query;
-    die($message);
-*/
-	}
+//Print general description last
+	echo "Description: $listing[description]<br><br>";
+	
 }
 
 ?>
