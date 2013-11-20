8d9ae3b6b4c376336ba9aadb9c4deb519c1664ca
Amanda <adoyle@smu.edu>
2013-11-19 23:14:44 -0600

    Android php functionality to view their listings

---
+++ Android_API/my_listings.php
@@ -0,0 +1,35 @@
+<?php
+
+    $con = mysql_connect("localhost", "listAdmin", "hermes");
+    if (!$con)
+    {
+        die('Could not connect: ' . mysql_error());
+    }
+
+    mysql_select_db("GregsList", $con) 
+        or die("Unable to select database:" . mysql_error());
+
+$query = "Select * from Listings where userID = '$_GET['userID']' order by listingID DESC";
+
+    $result = mysql_query($query);
+    $info = array();
+    while ($row = mysql_fetch_assoc($result)){
+        $listingID = $row['listingID'];
+        $userListingID = $row['userID'];
+        $title = $row['title'];
+        $dateListed = $row['dateListed'];
+        $category = $row['category'];
+        $price = $row['price'];
+        $description = $row['description'];
+        $listing = array("listingID" => $listingID, "userListingID" =>$userListingID, "title" => $title, "dateListed" => $dateListed, "category" => $category, "price" => $price, "description" => $description);
+        array_push($info, $listing);
+    }
+    $full_array= array("listings"=>$info);
+
+    $json = json_encode($full_array);
+    echo $json;
+    
+    mysql_close($con);
+
+
+?>
