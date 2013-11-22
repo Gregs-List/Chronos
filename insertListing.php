4f2d59d84306ebffb7672c6d66806b3f3dd6a52c
rsantoski <rsantoski@smu.edu>
2013-11-22 15:24:03 -0600

    Cleaning up insertListing.php

--- insertListing.php
+++ insertListing.php
@@ -15,8 +15,9 @@
 	$description = $_POST['description'];
 	$itemName = $_POST['itemName'];
 	
-	$last = mysql_query("SELECT photoID FROM Photos ORDER BY DESC 		LIMIT 1;"); 
-	$lastPhoto = mysql_fetch_array($last, MYSQL_ASSOC); 		$lastPhotoID = $lastPhoto['photoID'];
+	$last = mysql_query("SELECT photoID FROM Photos ORDER BY DESC LIMIT 1"); 
+	$lastPhoto = mysql_fetch_array($last, MYSQL_ASSOC); 		
+	$lastPhotoID = $lastPhoto['photoID'];
 	$photoID = $lastPhotoID + 1;
 
 	$listQuery = "INSERT INTO Listings VALUES(NULL, '$userID', '$title',NULL, '$category', '$desciption')";
@@ -24,37 +25,37 @@
 
 	if($category=="Bikes")
 	{
-		$catQuery = "INSERT INTO Bikes VALUES(last_insert_id(), '$_POST[bikeTypeID]', '$_POST[make]', '$_POST[model]'); \n";
+		$catQuery = "INSERT INTO Bikes VALUES(last_insert_id(), '$_POST[bikeTypeID]', '$_POST[make]', '$_POST[model]')";
 	}
 	
 	if($category=="Books")
 	{
-		$catQuery = "INSERT INTO Books VALUES(last_insert_id(), '$_POST[bookTypeID]', '$_POST[title]', '$_POST[author]', '$_POST[isbn]', '$_POST[assignedCourse]', '$_POST[conditionID]'); \n";
+		$catQuery = "INSERT INTO Books VALUES(last_insert_id(), '$_POST[bookTypeID]', '$_POST[title]', '$_POST[author]', '$_POST[isbn]', '$_POST[assignedCourse]', '$_POST[conditionID]')";
 	}
 
 	if($category=="Electronics")
 	{
-		$catQuery = "INSERT INTO Electronics VALUES(last_insert_id(), '$_POST[electronicsTypeID]', '$_POST[make]', '$_POST[model]', '$_POST[size]'); \n";
+		$catQuery = "INSERT INTO Electronics VALUES(last_insert_id(), '$_POST[electronicsTypeID]', '$_POST[make]', '$_POST[model]', '$_POST[size]')";
 	}
 
 	if($category=="Furniture")
 	{
-		$catQuery = "INSERT INTO Bikes VALUES(last_insert_id(), '$_POST[furnitureTypeID]', '$_POST[conditionID]'); \n";
+		$catQuery = "INSERT INTO Bikes VALUES(last_insert_id(), '$_POST[furnitureTypeID]', '$_POST[conditionID]')";
 	}
 
 	if($category=="Meetups")
 	{
-		$catQuery = "INSERT INTO Meetups VALUES(last_insert_id(), '$_POST[meetupTypeID]', '$_POST[location]', '$_POST[date]', '$_POST[time]'); \n";
+		$catQuery = "INSERT INTO Meetups VALUES(last_insert_id(), '$_POST[meetupTypeID]', '$_POST[location]', '$_POST[date]', '$_POST[time]')";
 	}
 
 	if($category=="Miscellaneous")
 	{
-		$catQuery = "INSERT INTO Miscellaneous VALUES(last_insert_id(), '); \n";
+		$catQuery = "INSERT INTO Miscellaneous VALUES(last_insert_id())";
 	}
 
 	if($category=="Rides")
 	{
-		$catQuery = "INSERT INTO Rides VALUES(last_insert_id(), '$_POST[leavingFrom]', '$_POST[goingTo]', '$_POST[departureDate]', '$_POST[departureTime]', '$_POST[returnDate]', '$_POST[returnTime]'); \n";
+		$catQuery = "INSERT INTO Rides VALUES(last_insert_id(), '$_POST[leavingFrom]', '$_POST[goingTo]', '$_POST[departureDate]', '$_POST[departureTime]', '$_POST[returnDate]', '$_POST[returnTime]')";
 	}
 
 
