3c28442a7445da8055217b5670fe7b19d21353c5
mwinslow@smu.edu <mwinslow@smu.edu>
2013-11-25 01:15:00 -0600

    Added a search file if searching just by category

---
+++ categorySearch.php
@@ -0,0 +1,28 @@
+<?php
+	session_start();
+	$con = mysql_connect("localhost", "listAdmin", "hermes");
+	if(!$con)
+	{
+		die('Could not connect: ' . mysql_error());
+	}
+	
+	mysql_select_db("GregsList", $con)
+		or die("Unable to select database:" . mysql_error());
+
+	$category = $_GET['category']; 
+
+	$query = "SELECT title, description, price FROM Listings WHERE category = '$category' ORDER BY dateListed DESC";
+		
+	$result = mysql_query($query);
+		
+	#converts to json
+	$rows = array();
+	while($r = mysql_fetch_assoc($result)) 
+	{
+    		$rows[] = $r;
+	}
+
+	print json_encode($rows);
+	#return json_encode($rows);
+	
+?>
