4c824f02e785522440194acab85431cb0c54c0ae
Amanda <adoyle@smu.edu>
2013-12-07 17:45:21 -0600

    logout

--- logout.php
+++ logout.php
@@ -1,5 +1,8 @@
 <?php 
 session_start();
-(session_status() == PHP_SESSION_NONE);
+session_status() == PHP_SESSION_NONE;
 header('Location:index.html');
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 ?>
