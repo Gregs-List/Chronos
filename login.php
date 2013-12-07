16c1c0fdc4ae761cdf6f9fe6dd3630821cbb9adf
Amanda <adoyle@smu.edu>
2013-12-07 16:40:55 -0600

    changed login structure completely

--- login.php
+++ login.php
@@ -1,34 +1,39 @@
 <?php
 
     $con = mysql_connect("localhost", "listAdmin", "hermes");
-    if (!$con)
-    {
-        die('Could not connect: ' . mysql_error());
-    }
+	if (!$con)
+	{
+		die('Could not connect: ' . mysql_error());
+	}
 
     mysql_select_db("GregsList", $con) 
         or die("Unable to select database:" . mysql_error());
 
-		//Revised fntion to check login credentials
-		$user = mysql_real_escape_string($_POST['email']);
-		$pass = mysql_real_escape_string($_POST['password']);
-		$userCheck = mysql_query("SELECT * FROM Users WHERE email='$user' and pword='$pass'")
-    or die (mysql_error());
-		$userExists = mysql_fetch_array($userCheck, MYSQL_ASSOC);
-		if(mysql_num_rows($userCheck)==1)
-		{
-			$userExists = mysql_fetch_array($userCheck, MYSQL_ASSOC);
-			session_start();
-			echo $userExists['userID'];
-			$_SESSION['userID'] = $userExists['userID'];
-			//echo $_SESSION['userID'];
-			//header('Location: home.html');
-		}
-		else
-		{
-			echo "<META HTTP-EQUIV=REFRESH CONTENT='0; URL=index.html'><script type='text/javascript'>alert('Invalid username or password. If you do not have an account, register on the right.');</script>";
-		}
-		
+    $password = $_GET['password'];
+    $email = $_GET['email'];
 
+    $query = "select * from Users where email = '$email'";
+	$result = mysql_query($query);
+
+	while ($row = mysql_fetch_assoc($result)){
+        $id = $row['userID'];
+        $name = $row['fullName'];
+        $location = $row['location'];
+        $phoneNumber = $row['phoneNumber'];
+    }
+    
+    session_start();
+    $_SESSION['userID']=$id;
+	$query = "select pword from Users where userID ='$id'";
+    $result2 = mysql_query($query);
+    while ($row = mysql_fetch_assoc($result2)){
+    	$database_password = $row ['pword'];
+    }
+    if($password != $database_password){
+   		echo 'error';
+    }
+    else{
+   		echo $id;
+        }
     mysql_close($con);
-?>
+    ?>
